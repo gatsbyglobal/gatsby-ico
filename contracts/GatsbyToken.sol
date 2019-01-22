877823a758fedd3901933f93d883d0f1f29ecde2
junwooji <junwoo@finda.co.kr>
2019-01-23 00:14:02 +0900

    add burn, pause feature

--- contracts/GatsbyToken.sol
+++ contracts/GatsbyToken.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract GatsbyToken is ERC20, ERC20Detailed {
+contract GatsbyToken is ERC20Pausable, ERC20Detailed, ERC20Burnable {
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
 
